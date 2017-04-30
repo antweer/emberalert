f91ec9bc9b8d6416b0c5a865a4cbe7879b30a40e
antweer <trajwani@gmail.com>
2017-04-30 12:07:13 -0500

    changed config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-var dataUrl = '/data/MODIS_C6_Global_24h.csv';
+var dataUrl = './data/MODIS_C6_Global_24h.csv';
 var maxZoom = 9;
 var fieldSeparator = ',';
 var baseUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
-var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+var baseAttribution = 'Data, imagery and map information provided by <a href="https://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
 var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 25};
 var labelColumn = "Name";
