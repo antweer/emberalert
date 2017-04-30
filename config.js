d990939a4cbd971f99549a3f673044050c202adb
antweer <trajwani@gmail.com>
2017-04-30 15:20:25 -0500

    Messing with clusters

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ var fieldSeparator = ',';
 var baseUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
 var baseAttribution = 'Data, imagery and map information provided by <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
-var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 1};
+var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 25};
 var labelColumn = "Name";
 var opacity = 1.0;
