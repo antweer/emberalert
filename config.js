352d90dcdf9154544fa42e3dc3f441a47b03b707
antweer <trajwani@gmail.com>
2017-05-04 16:20:44 -0500

    Updating index page and data

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var dataUrl = 'data/MODIS_C6_Global_24h.csv';
 var maxZoom = 9;
 var fieldSeparator = ',';
 var baseUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
-var baseAttribution = 'Data, imagery and map information provided by <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+var baseAttribution = 'Data, imagery, and map information provided by <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and <a href="https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms/active-fire-data">NASA</a>. <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
 var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 25};
 var labelColumn = "Name";
